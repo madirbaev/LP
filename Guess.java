cc90b64ac4af482062a6d155ebd577298f661147
madirbaev <maderbaev1997@gmail.com>
2026-04-23 23:23:26 +0700

    Создаём игру на угадывание буквы

---
+++ Guess.java
@@ -0,0 +1,24 @@
+// Игра в угадывание буквы.
+class Guess {
+	public static void main(String[] args)
+			throws java.io.IOException {
+		char ch, ignore, answer = 'K'; // Задумываем букву.
+		do {
+			System.out.println("Задумана буква между А и Z.");
+			System.out.print("Попробуйте ее угадать: ");
+			// Прочитать символ.
+			ch = (char) System.in.read();
+			// Отбросить все остальные символы из буфера ввода,
+			do {
+				ignore = (char) System.in.read();
+			} while(ignore != '\n');
+			if(ch == answer) System.out.println("** Правильно **");
+			else {
+				System.out.print("...Увы , не угадали. Задуманная буква находится ");
+				if(ch < answer) System.out.println("дальше по алфавиту.");
+				else System.out.println("ближе по алфавиту.");
+				System.out.println("Повторите попытку!\n");
+			}
+		} while(answer != ch);
+	}
+}
