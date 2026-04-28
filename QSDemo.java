3b6bfb3bef8c2d5da63b0244afffb800cd772b00
madirbaev <maderbaev1997@gmail.com>
2026-04-28 22:43:08 +0700

    Быстрая сортировка

---
+++ QSDemo.java
@@ -0,0 +1,42 @@
+// Упражнение 6.3. Простая версия быстрой сортировки.
+class Quicksort {
+	// Настраивает вызов фактического метода быстрой сортировки.
+	static void qsort(char[] items) {
+		qs(items, 0, items.length-1);
+	}
+	// Рекурсивная версия быстрой сортировки для символов.
+	private static void qs(char[] items, int left, int right) {
+		int i, j;
+		char x, y;
+		i = left; j = right;
+		x = items[(left+right)/2];
+		do {
+			while((items[i] < x) && (i < right)) i++;
+			while((x < items[j]) && (j > left)) j--;
+			if(i <= j) {
+				y = items[i];
+				items[i] = items[j];
+				items[j] = y;
+				i++; j--;
+			}
+		} while(i <= j);
+		if(left < j) qs(items, left, j);
+		if(i < right) qs(items, i, right);
+	}
+}
+class QSDemo {
+	public static void main(String[] args) {
+		char[] a = { 'd', 'x', 'a', 'r', 'p', 'j', 'i' };
+		int i;
+		System.out.print("Исходный массив: ");
+		for(i=0; i < a.length; i++)
+			System.out.print(a[i]);
+		System.out.println();
+
+		// Отсортировать массив.
+		Quicksort.qsort(a);
+		System.out.print("Отсортированный массив: ");
+		for(i=0; i < a.length; i++)
+			System.out.print(a[i]);
+	}
+}
