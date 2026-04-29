fc52375ed87efde2a6f805f8d7ebc887ee172fc4
madirbaev <maderbaev1997@gmail.com>
2026-04-30 00:52:58 +0700

    Используем модификатор protected

---
+++ bookpackext/BookDemo.java
@@ -0,0 +1,31 @@
+// Объявление защищенными переменных экземпляра в классе Book.
+package bookpackext;
+public class Book {
+	// теперь эти члены защищенные
+	protected String title;
+	protected String author;
+	protected int pubDate;
+	public Book(String t, String a, int d) {
+		title = t;
+		author = a;
+		pubDate = d;
+	}
+	public void show() {
+		System.out.println(title);
+		System.out.println(author);
+		System.out.println(pubDate);
+		System.out.println();
+	}
+}
+public class BookDemo {
+	public static void main(String[] args) {
+		Book[] books = new Book[5];
+		books[0] = new Book("Java: A Beginner's Guide", "Schildt", 2022);
+		books[1] = new Book("Java: The Complete Reference", "Schildt", 2022);
+		books[2] = new Book("1984", "Orwell", 1949);
+		books[3] = new Book("Red Storm Rising", "Clancy", 1986);
+		books[4] = new Book("On the Road", "Kerouac", 1955);
+		for(int i=0; i < books.length; i++)
+			books[i].show();
+	}
+}
---
+++ bookpackext/ProtectDemo.java
@@ -0,0 +1,41 @@
+// Демонстрация применения protected.
+package bookpackext;
+class ExtBook extends bookpack.Book {
+	private String publisher;
+	public ExtBook(String t, String a, int d, String p) {
+		super(t, a, d);
+		publisher = p;
+	}
+	public void show() {
+		super.show();
+		System.out.println(publisher);
+		System.out.println();
+	}
+	public String getPublisher() { return publisher; }
+	public void setPublisher(String p) { publisher = p; }
+
+	/* Разрешено, потому что подкласс имеет доступ к защищенным членам. */
+	public String getTitle() { return title; }
+	public void setTitle(String t) { title = t; }
+	public String getAuthor() { return author; }
+	public void setAuthor(String a) { author = a; }
+	public int getPubDate() { return pubDate; }
+	public void setPubDate(int d) { pubDate = d; }
+}
+class ProtectDemo {
+	public static void main(String[] args) {
+		ExtBook[] books = new ExtBook[5];
+		books[0] = new ExtBook("Java: A Beginner's Guide", "Schildt", 2022, "McGraw Hill");
+		books[1] = new ExtBook("Java: The Complete Reference", "Schildt", 2022, "McGraw Hill");
+		books[2] = new ExtBook("1984", "Orwell", 1949, "Harcourt Brace Jovanovich");
+		books[3] = new ExtBook("Red Storm Rising", "Clancy", 1986, "Putnam");
+		books[4] = new ExtBook("On the Road", "Kerouac", 1955, "Viking");
+		for(int i=0; i < books.length; i++) books[i].show();
+		// Найти книги по автору.
+		System.out.println("Все книги, автор которых Schildt:");
+		for(int i=0; i < books.length; i++)
+			if(books[i].getAuthor() == "Schildt")
+				System.out.println(books[i].getTitle());
+		// books[0].title = "test title"; // Ошибка - недоступно.
+	}
+} 
