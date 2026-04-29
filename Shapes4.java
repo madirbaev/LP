51274b3abd301614c5cfac8e2d4d9ca5ecd6caca
madirbaev <maderbaev1997@gmail.com>
2026-04-29 23:08:25 +0700

    Расширение классов TwoDShape и Triangle

--- Shapes4.java
+++ Shapes4.java
@@ -1,11 +1,19 @@
-// Добавление конструкторов к TwoDShape.
+// Добавление дополнительных конструкторов к TwoDShape.
 class TwoDShape {
 	private double width;
 	private double height;
+	// Стандартный конструктор.
+	TwoDShape() {
+		width = height = 0.0;
+	}
 	// Параметризованный конструктор.
 	TwoDShape(double w, double h) {
 		width = w;
 		height = h;
+	}
+	// Конструктор объекта с одинаковыми шириной и высотой.
+	TwoDShape(double х) {
+		width = height = х;
 	}
 	// Методы доступа для width и height.
 	double getWidth() { return width; }
@@ -19,9 +27,20 @@ class TwoDShape {
 // Подкласс TwoDShape для представления треугольников.
 class Triangle extends TwoDShape {
 	private String style;
+	// Стандартный конструктор.
+	Triangle() {
+		super();
+		style = "отсутствует";
+	}
+	// Конструктор.
 	Triangle(String s, double w, double h) {
 		super(w, h); // вызов конструктора суперкласса
 		style = s;
+	}
+	// Конструктор с одним аргументом.
+	Triangle(double х) {
+		super(х); // вызов конструктора суперкласса
+		style = "закрашенный";
 	}
 	double area() {
 		return getWidth() * getHeight() / 2;
@@ -32,8 +51,10 @@ class Triangle extends TwoDShape {
 }
 class Shapes4 {
 	public static void main(String[] args) {
-		Triangle tl = new Triangle("закрашенный", 4.0, 4.0);
+		Triangle tl = new Triangle();
 		Triangle t2 = new Triangle("контурный", 8.0, 12.0);
+		Triangle t3 = new Triangle(4.0);
+		tl = t2;
 		System.out.println("Информация об объекте tl: ");
 		tl.showStyle();
 		tl.showDim();
@@ -43,5 +64,11 @@ class Shapes4 {
 		t2.showStyle();
 		t2.showDim();
 		System.out.println("Площадь: " + t2.area());
+		System.out.println();
+		System.out.println("Информация об объекте t3: ");
+		t3.showStyle();
+		t3.showDim();
+		System.out.println("Площадь: " + t3.area());
+		System.out.println();
 	}
 }
